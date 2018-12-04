7d9273062ef8e984d1512494b0d585375837f3b7
Rasmus Drotved Laursen <rasm781y@edu.easj.dk>
2018-12-04 14:02:46 +0100

    Added Comments and Explanations to pretty much everything

--- QuizManager/ViewModel/ViewModelBasement.cs
+++ QuizManager/ViewModel/ViewModelBasement.cs
@@ -26,13 +26,16 @@ namespace QuizManager.ViewModel
         }
         #endregion
 
+        
         #region Instancefields
-
+        
+        //disse 2 felter tilhører tilsvarende properties i Properties Tabben
         private ObservableCollection<ModelGroup> _groups;
         private ModelGroup _selectedGroup;
 
         #endregion
 
+        //properties som skal bruges til at oprette nye grupper fra GUI
         #region ModelGroupProps
         //By Mosbæk
 
@@ -49,13 +52,24 @@ namespace QuizManager.ViewModel
         public string AllPayed { get; set; }
 
         public int NumberOfAttendingParticipants { get; set; }
+
+        //end Mosbæk
         #endregion
 
+        //property der har med Dato at gøre
         public string Date { get; set; }
 
-
+        
         #region Properties
+        
+        //SelectedItem property som kan bruges til at få fat i properties hos en valgt ModelDate
+        public ModelDate SelectedItem { get; set; }
 
+        // observable collection i property form med ModelDateCollection 
+        public ObservableCollection<ModelDateCollection> DateCollection { get; set; }
+        
+
+        //property der har med SelectedGroup at gøre, som kan vælges fra GridView i ViewBasement
         public ModelGroup SelectedGroup
         {
             get { return _selectedGroup; }
@@ -66,8 +80,7 @@ namespace QuizManager.ViewModel
             }
         }
 
-        public ObservableCollection<ModelDateCollection> DateCollection { get; set; }
-
+        //observable collection af ModelGroups
         public ObservableCollection<ModelGroup> BasementGroups
         {
             get { return _groups; }
@@ -76,21 +89,18 @@ namespace QuizManager.ViewModel
 
         #endregion
 
-        #region ModelDateProps
-
-        public ModelDate SelectedItem { get; set; }
-
-
-        #endregion
-
+       
         #region Empty Constructor
 
         public ViewModelBasement()
         {
-
+            //her skabes ny DateCollection og der tilføjes en ModelDateCollection til den. 
            DateCollection = new ObservableCollection<ModelDateCollection>();
            DateCollection.Add(new ModelDateCollection());
 
+            //som ovenstående, bortset fra at her så oprettes en instans af en collection af groups
+            //samt der kaldes en metode som tilføjer 3 test groups.
+            //nederst sættes propertien AllPaid til true, hos objektet på plads [0] i group collectionen
             _groups = new ObservableCollection<ModelGroup>();
             AddTest();
             _groups[0].AllPaid = true;
@@ -104,23 +114,21 @@ namespace QuizManager.ViewModel
 
         public void AddSeat()
         {
-            //skal ha fat i TotalSeats i ModelDate
+            //skal ha fat i TotalSeats i ModelDate og pluse med 1. findes gennem SelectedItem af ModelDates
             SelectedItem.TotalSeats++;
+            
         }
 
         public void SubtractSeat()
         {
-            //skal ha fat i TotalSeats i ModelDate
+            //skal ha fat i TotalSeats i ModelDate og minuse med 1. findes gennem selectedItem af ModelDates
             SelectedItem.TotalSeats--;
         }
 
         
-
-
-
-
         #endregion
 
+        //metode der skaber 3 testgrupper, med yderst opfindsomme navne
         public void AddTest()
         {
             _groups.Add(new ModelGroup("Russerne", "10203040", 7, 1));
@@ -128,8 +136,9 @@ namespace QuizManager.ViewModel
             _groups.Add(new ModelGroup("Gede Hviskerne", "30405060", 12, 3));
         }
 
+        //skal måske bruges senere. spørg Laura. DON'T DELETE
         #region Udkommenteret Kode
-
+        
         //public void MakeGroupList(string date)
         //{
         //    if (_groups.Count < ModelEventsBasementSingleton.Instance.BasementModelDateCollection.GetDate(date).Groups
